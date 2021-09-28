8bfa4a6840fb39c5dfbfbf5e0ab5aeb05a846ada
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-28 11:07:48 +0300

    geting and seeing posts

--- public/front.js
+++ public/front.js
@@ -1,6 +1,7 @@
 const formEl = document.getElementById('form');
 const authorEl = document.getElementById('author');
 const bodyEl = document.getElementById('text');
+const postsListEl = document.getElementById('posts-list');
 
 const url = 'http://localhost:3000';
 
@@ -25,6 +26,19 @@ formEl.addEventListener('submit', (e) => {
     .catch((err) => console.log('err', err));
 });
 
+const renderPosts = (postsArr, destination) => {
+  destination.innerHTML = '';
+  postsArr.forEach((el) => {
+    const liEl = `
+      <li data-id=${el.id} >
+        <h4>Author: ${el.author}</h4>
+        <p>${el.body}</p>
+      </li>
+    `;
+    destination.innerHTML += liEl;
+  });
+};
+
 const getPosts = async () => {
   console.log('getPosts running');
   try {
@@ -32,9 +46,11 @@ const getPosts = async () => {
     const data = await resp.json();
     if (data.msg === 'success') {
       console.log('posts', data.posts);
+      renderPosts(data.posts, postsListEl);
     }
   } catch (error) {
     console.log(error);
   }
 };
+
 getPosts();
